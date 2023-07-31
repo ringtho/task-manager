f36b21bad1c2ecaa0617625e435b9442ee43d0e0
Smith Ringtho <sringtho@gmail.com>
2023-07-31 21:16:31 +0300

    Added routes abd controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,23 @@
-const getAllTasks = (req, res) => {
+const getTasks = (req, res) => {
     res.send('All Items')
 }
 
-module.exports = { getAllTasks }
+const createTask = (req, res) => {
+    res.send('create Task')
+}
+
+const getTask = (req, res) => {
+    res.send('Single Task')
+}
+
+const updateTask = (req, res) => {
+    res.send('Update Task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('Delete Task')
+}
+
+
+
+module.exports = { getTasks, getTask, createTask, updateTask, deleteTask }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const { getAllTasks } = require('../controllers/tasks')
+const { 
+    getTasks, 
+    getTask, 
+    createTask, 
+    updateTask, 
+    deleteTask 
+} = require('../controllers/tasks')
 
 
 router.route('/')
-    .get(getAllTasks)
+    .get(getTasks)
+    .post(createTask)
+
+router.route('/:id')
+    .get(getTask)
+    .patch(updateTask)
+    .delete(deleteTask)
 
 module.exports = router
