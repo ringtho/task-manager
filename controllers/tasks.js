b0d8409481601f8c2b6c071c376754554a6b4b09
Smith Ringtho <sringtho@gmail.com>
2023-08-03 10:36:45 +0300

    Added custom error handlers with the Error class

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task')
 const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
 const getTasks = asyncWrapper (async (req, res) => {
     const tasks = await Task.find({})
@@ -11,10 +12,10 @@ const createTask = asyncWrapper (async (req, res) => {
     res.status(201).json({task})
 })
 
-const getTask = asyncWrapper (async (req, res) => {
+const getTask = asyncWrapper (async (req, res, next) => {
     const task = await Task.findById(req.params.id)
     if(!task) {
-        return res.status(404).json({msg: `No task with id: ${req.params.id}`})
+        return next(createCustomError(`No task with id: ${req.params.id}`, 404))
     }
     res.status(200).json({ task })
 })
@@ -25,7 +26,7 @@ const updateTask = asyncWrapper (async (req, res) => {
         new: true, runValidators: true
     })
     if(!task) {
-        return res.status(404).json({msg: `No task with id: ${taskId}`})
+        return next(createCustomError(`No task with id: ${req.params.id}`, 404))
     }
     res.status(200).json({ task })
 })
@@ -34,7 +35,7 @@ const deleteTask = asyncWrapper (async (req, res) => {
     const {id: taskId} = req.params
     const task = await Task.findOneAndDelete({_id: taskId})
     if(!task) {
-        return res.status(404).json({msg: `No task with id: ${taskId}`})
+        return next(createCustomError(`No task with id: ${req.params.id}`, 404))
     }
     res.status(200).json({ task })
 })
