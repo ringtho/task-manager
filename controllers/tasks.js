1bc0ef3f5f861a48a86e51280037691c7d58a9d1
Smith Ringtho <sringtho@gmail.com>
2023-08-03 09:39:13 +0300

    Added async wrapper to reduce the try catch block redundancy

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,10 @@
 const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async')
 
-const getTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.status(200).json({tasks, amount: tasks.length })
-    } catch(err) {
-        res.status(500).json({ msg: err })
-    }
-}
+const getTasks = asyncWrapper (async (req, res) => {
+    const tasks = await Task.find({})
+    res.status(200).json({tasks, amount: tasks.length })
+})
 
 const createTask = async (req, res) => {
     try {
